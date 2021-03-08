36359b2c83ba4a91a366124549ae21935a14b162
Cooper Stansbury <cstansbu@gl-login2.arc-ts.umich.edu>
2021-03-08 09:19:26 -0500

    update colored figures

--- notebooks/plot_embeddings.py
+++ notebooks/plot_embeddings.py
@@ -26,23 +26,24 @@ import utils
 ROOT_DIR = "/scratch/indikar_root/indikar1/shared_data/cstansbu_rotation/b_cell_data/outputs/"
 
 for dataset in ['D1-Na', 'D1-Nb', 'D1-M',
-                'D2-N', 'D1-M',
+                'D2-N', 'D2-M',
                 'D3-N', 'D3-M']:
 
-    df = utils.load_embed_and_feat(dataset, ROOT_DIR)
-    print(dataset, df.shape)
 
-    fig = utils.build_fig(y_lab="UMAP-2", 
-                          x_lab="UMAP-1", 
-                          title=f'Sample from {dataset} Clusters')
+    if dataset == 'D2-M':
+        df = utils.load_embed_and_feat(dataset, ROOT_DIR)
+        print(dataset, df.shape)
 
-    sns.scatterplot(x=df['UMAP_1'], 
-                    y=df['UMAP_2'], 
-                    hue=df['vFamilyName'],
-                    alpha=0.5)
+        fig = utils.build_fig(y_lab="UMAP-2", 
+                            x_lab="UMAP-1", 
+                            title=f'Sample from {dataset} Clusters')
 
-    break
-    # outpath = f"../figures/{dataset}_UMAP_by_Family.png"
-    # plt.savefig(outpath, bbox_inches = 'tight')
+        sns.scatterplot(x=df['UMAP_1'], 
+                        y=df['UMAP_2'], 
+                        hue=df['vFamilyName'],
+                        alpha=0.5)
+
+    outpath = f"../figures/{dataset}_UMAP_by_Family.png"
+    plt.savefig(outpath, bbox_inches = 'tight')
 
 # %%
